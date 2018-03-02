1d17b15f9db1289cd7938d8d6aa90186e523cc4c
Антон <>
2018-03-02 15:28:56 +0100

    footer updated + about.php added

--- include/footer.php
+++ include/footer.php
@@ -1,1 +1,8 @@
-footer
+<!-- footer -->
+<div class="footerPlain">
+	<div class="container">
+		<div>
+			<h3>Footer Text</h3>
+		</div>
+	</div>
+</div>
---
+++ include/pages/about.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('../../templating/meta-config.html'); ?>
+</head>
+<body>
+  <?php include('../header.php'); // echo "<br />\n";?>
+  <?php include('../menu.php'); ?>
+    <div class="container">
+    <!-- Content  -->
+    <div class="trenn"></div>
+<div class="container text-center">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-5"><img class="mb-4" src="../../static/icons/secondary/sports-45.svg" height="48px">
+            <h3 class="h5 mb-2">Loads quickly</h3>
+            <p class="text-secondary mb-3 lead">Super fast loading speed. Only pure Bootstrap 4, and no additional third-party plugins</p>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-5"><img class="mb-4" src="../../static/icons/secondary/interface-32.svg" height="48px">
+            <h3 class="h5 mb-2">Minimal and clean design</h3>
+            <p class="text-secondary mb-3 lead">Modern design is a top priority. With Replacer anyone can create stunning websites</p>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-5"><img class="mb-4" src="../../static/icons/secondary/devices-37.svg" height="48px">
+            <h3 class="h5 mb-2">Easy to understand</h3>
+            <p class="text-secondary mb-3 lead">To master Replacer you only need official BS4 documentation, no more</p>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-5"><img class="mb-4" src="../../static/icons/secondary/communications-16.svg" height="48px">
+            <h3 class="h5 mb-2">6 months technical support</h3>
+            <p class="text-secondary mb-3 lead">We are here for you to provide excellent support for 6 months <a href="mailto:hello@getreplacer.com">hello@getreplacer.com</a></p>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-5"><img class="mb-4" src="../../static/icons/secondary/e-commerce-31.svg" height="48px">
+            <h3 class="h5 mb-2">Free updates</h3>
+            <p class="text-secondary mb-3 lead">Constantly growing and updatable HTML template that follows native bootstrap 4 features</p>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-5"><img class="mb-4" src="../../static/icons/secondary/interface-55.svg" height="48px">
+            <h3 class="h5 mb-2">Easy to customize</h3>
+            <p class="text-secondary mb-3 lead">Just add few lines of CSS code to get your own design</p>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-5"><img class="mb-4" src="../../static/icons/secondary/devices-38.svg" height="48px">
+            <h3 class="h5 mb-2">Mobile first</h3>
+            <p class="text-secondary mb-3 lead">All components and templates are responsive on any devices</p>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-5"><img class="mb-4" src="../../static/icons/secondary/numbers-16.svg" height="48px">
+            <h3 class="h5 mb-2">Free fonts</h3>
+            <p class="text-secondary mb-3 lead">Google Fonts and Font Awesome are included</p>
+          </div>
+        </div>
+      </div>
+    <!-- End of Content -->
+</div>
+    <?php include('../footer.php'); ?>
+    <?php include('../../templating/support-scripts.html'); ?>
+  </body>
+</html>
