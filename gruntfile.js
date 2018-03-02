85e28aa939843448569e0bfbfec558be21619fe0
Антон <>
2018-03-02 14:15:28 +0100

    node modules deleted

--- gruntfile.js
+++ gruntfile.js
@@ -1,29 +1,41 @@
-module.exports = function (grunt) {
+'use strict';
+var path = require('path');
+function absolutePath(file) {
+  return path.join(__dirname, file);
+}
+module.exports = function(grunt) {
   grunt.initConfig({
-    // Watch task config
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+
+        files: {
+          './static/css/main.css' : './static/scss/main.scss'
+        }
+      }
+    },
     watch: {
-      sass: {
-        files: "static/scss/*.scss",
+      css: {
+        files: '**/*.scss',
         tasks: ['sass']
       }
     },
-    // SASS task config
-    sass: {
-        dist: {
-          options: {
-            debugInfo:true,
-            style: 'expanded',
-            precision: 8,
-noCache: true
-          }, 
-            files: {
-                // destination         // source file
-                "static/css/main.css" : "static/scss/main.scss"
-            }
-        }
-    },
-  });
-
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      absolute: {
+        files: [{
+          src: [
+            './static/css/main.css'
+          ].map(absolutePath),
+          dest: absolutePath('./static/css/main.min.css')
+        }]
+}
+  }
+});
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-};
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default',['watch']);
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ Not working --> Sass
 
 # First try to create a full php temlating
 # Grunt as main builder
+in order to work, grunt needs ruby and sass installed
