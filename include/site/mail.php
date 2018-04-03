fe43d26cead48f3cdec90781b03d81a82b4da561
siemensr <priman2004@mail.ru>
2018-04-03 22:21:37 +0200

    email send function added

---
+++ include/site/mail.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+if(!isset($_SESSION['userid'])) {
+    die('The contents are available only after <a href="../login.php">login</a>');
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Send Email</title>
+</head>
+<body>
+<div class="container">
+<h1 style="margin-top:10%;">Enter the mail text:</h1><br/>
+<br/>
+
+<form action="send.php" method="post">
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Email address</label>
+       <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="name" required="required">
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlTextarea1">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="phone" required="required"></textarea><br/>
+        <input class="btn btn-lg btn-primary" type="submit" value="Send"/>
+    </div>
+</form>
+</div>
+
+<?php
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+    $to = $_POST['name']; //Почта получателя, через запятую можно указать сколько угодно адресов
+    $subject = 'Обратный звонок'; //Загаловок сообщения
+    $message = '
+           <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+    $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+    mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
+            var form_data = $(this).serialize(); //собераем все данные из формы
+            $.ajax({
+                type: "POST", //Метод отправки
+                url: "/send.php", //путь до php фаила отправителя
+                data: form_data,
+                success: function() {
+                    //код в этом блоке выполняется при успешной отправке сообщения
+                    alert("Ваше сообщение отпрвлено!");
+                    }});
+        });
+    });
+</script>
+</body>
+</html>
